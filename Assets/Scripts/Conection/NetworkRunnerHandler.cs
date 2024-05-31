f066261720213c2d682e3267d8614047095576ac
Zebado <sbasualdo.antares@gmail.com>
2024-05-31 16:21:29 -0300

    Canvas finished
    Clase 24 - 05 1 hora 1 minuto 10 segundos

--- Assets/Scripts/Conection/NetworkRunnerHandler.cs
+++ Assets/Scripts/Conection/NetworkRunnerHandler.cs
@@ -1,8 +1,11 @@
-using Fusion;
-using Fusion.Sockets;
 using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
+using Fusion;
+using Fusion.Sockets;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class NetworkRunnerHandler : MonoBehaviour, INetworkRunnerCallbacks
 {
@@ -10,15 +13,90 @@ public class NetworkRunnerHandler : MonoBehaviour, INetworkRunnerCallbacks
 
     NetworkRunner _currentRunner;
 
+    public event Action OnJoinedLobby = delegate { };
+
+    private void Start()
+    {
+        JoinLobby();
+    }
+
+    #region Join / Create Game
+
+    public async void CreateGame(string sessionName, string sceneName)
+    {
+        await InitializeGame(GameMode.Host, sessionName, SceneUtility.GetBuildIndexByScenePath($"Scenes/{sceneName}"));
+    }
+    public async void JoinGame(SessionInfo sessionInfo)
+    {
+        await InitializeGame(GameMode.Client, sessionInfo.Name, SceneManager.GetActiveScene().buildIndex);
+    }
+
+    async Task InitializeGame(GameMode gameMode, string sessionName, int sceneIndex)
+    {
+        _currentRunner.ProvideInput = true;
+
+        var result = await _currentRunner.StartGame(new StartGameArgs()
+        {
+            GameMode = gameMode,
+            Scene = SceneRef.FromIndex(sceneIndex),
+            SessionName = sessionName
+        });
+
+        if (!result.Ok)
+        {
+            Debug.LogError("[Custom error] Unable to start game");
+        }
+        else
+        {
+            Debug.Log("[Custom Msg] Game started");
+        }
+    }
+
+    #endregion
+
+    #region Lobby
     public void JoinLobby()
     {
+        if (_currentRunner) Destroy(_currentRunner.gameObject);
 
+        _currentRunner = Instantiate(_runnerPrefab);
+
+        _currentRunner.AddCallbacks(this);
+
+        JoinLobbyAsync();
     }
+     
+    async void JoinLobbyAsync()
+    {
+        var result = await _currentRunner.JoinSessionLobby(SessionLobby.Custom, "Normal Lobby");
 
+        if (!result.Ok)
+        {
+            Debug.LogError("[Custom error] UNable to Join lobby");
+        }
+        else
+        {
+            Debug.Log("[Custom Msg] Joined Lobby");
+            OnJoinedLobby();
+
+            //CreateGame("x", "Game");
+        }
+    }
+    #endregion
+
+    #region Used Runner Callbacks
     public void OnSessionListUpdated(NetworkRunner runner, List<SessionInfo> sessionList)
     {
+        if (sessionList.Count == 0) return;
 
+        var session = sessionList[0];
+
+        Debug.Log($"[Custom msg] joining {session.Name}");
+
+        JoinGame(session);
     }
+    #endregion
+
 
     #region Unused Runner Callbacks
 
