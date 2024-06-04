e3b43108c8248cc32e3ae548dad857a367e05b37
Zebado <sbasualdo.antares@gmail.com>
2024-06-04 16:19:13 -0300

    Update
    1 hora 7 minutos 40 segundos

--- Assets/Scripts/Main Menu/MainMenuHandler.cs
+++ Assets/Scripts/Main Menu/MainMenuHandler.cs
@@ -1,18 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MainMenuHandler : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] NetworkRunnerHandler _networkHandler;
+
+    [Header("Panels")]
+    [SerializeField] GameObject _initialPanel;
+    [SerializeField] GameObject _sessionBrowserPanel;
+    [SerializeField] GameObject _hostGamePanel;
+    [SerializeField] GameObject _statusPanel;
+
+    [Header("Buttons")]
+    [SerializeField] Button _joinLobbyButton;
+    [SerializeField] Button _gotToHostPanelButton;
+    [SerializeField] Button _hostButton;
+
+    [Header("InputFields")]
+    [SerializeField] TMP_InputField _hostsessionName;
+
+    [Header("Texts")]
+    [SerializeField] TMP_Text _statusText;
     void Start()
     {
-        
+        _joinLobbyButton.onClick.AddListener(Button_JoinLobby);
+        _gotToHostPanelButton.onClick.AddListener(Button_ShowHostPanel);
+        _hostButton.onClick.AddListener(Button_CreateGameSession);
+
+        _networkHandler.OnJoinedLobby += () =>
+        {
+            _statusPanel.SetActive(false);
+            _sessionBrowserPanel.SetActive(true);
+        };
     }
 
-    // Update is called once per frame
-    void Update()
+    void Button_JoinLobby()
     {
-        
+
+    }
+    void Button_ShowHostPanel()
+    {
+
+    } 
+    void Button_CreateGameSession()
+    {
+
     }
 }
