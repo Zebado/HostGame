55a3c04c656bdc667da053de1ba943c7613bcbca
taio1000 <tadeoelli@hotmail.com>
2024-06-15 22:28:11 -0300

    commit

---
+++ Assets/Tiled/Tilesets/TileSet.tsx
@@ -0,0 +1,163 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="TileSet" tilewidth="256" tileheight="256" tilecount="52" columns="0" tilerendersize="grid">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <properties>
+  <property name="DeleteTiles" type="bool" value="true"/>
+ </properties>
+ <tile id="102">
+  <image width="256" height="256" source="../../Art/Tiles/Acid (1).png"/>
+ </tile>
+ <tile id="103">
+  <image width="256" height="256" source="../../Art/Tiles/Acid (2).png"/>
+ </tile>
+ <tile id="104">
+  <image width="256" height="256" source="../../Art/Tiles/BGTile (1).png"/>
+ </tile>
+ <tile id="105">
+  <image width="256" height="256" source="../../Art/Tiles/BGTile (2).png"/>
+ </tile>
+ <tile id="106">
+  <image width="256" height="256" source="../../Art/Tiles/BGTile (3).png"/>
+ </tile>
+ <tile id="107">
+  <image width="256" height="256" source="../../Art/Tiles/BGTile (4).png"/>
+ </tile>
+ <tile id="108">
+  <image width="256" height="256" source="../../Art/Tiles/BGTile (5).png"/>
+ </tile>
+ <tile id="109">
+  <image width="256" height="256" source="../../Art/Tiles/BGTile (6).png"/>
+ </tile>
+ <tile id="110">
+  <image width="256" height="256" source="../../Art/Tiles/BGTile (7).png"/>
+ </tile>
+ <tile id="111">
+  <image width="256" height="256" source="../../Art/Tiles/Fence (1).png"/>
+ </tile>
+ <tile id="112">
+  <image width="256" height="256" source="../../Art/Tiles/Fence (2).png"/>
+ </tile>
+ <tile id="113">
+  <image width="256" height="256" source="../../Art/Tiles/Fence (3).png"/>
+ </tile>
+ <tile id="114">
+  <image width="256" height="256" source="../../Art/Tiles/Spike.png"/>
+ </tile>
+ <tile id="115">
+  <image width="256" height="256" source="../../Art/Tiles/Tile (1).png"/>
+ </tile>
+ <tile id="116">
+  <image width="256" height="256" source="../../Art/Tiles/Tile (2).png"/>
+ </tile>
+ <tile id="117">
+  <image width="256" height="256" source="../../Art/Tiles/Tile (3).png"/>
+ </tile>
+ <tile id="118">
+  <image width="256" height="256" source="../../Art/Tiles/Tile (4).png"/>
+ </tile>
+ <tile id="119">
+  <image width="256" height="256" source="../../Art/Tiles/Tile (5).png"/>
+ </tile>
+ <tile id="120">
+  <image width="256" height="256" source="../../Art/Tiles/Tile (6).png"/>
+ </tile>
+ <tile id="121">
+  <image width="256" height="256" source="../../Art/Tiles/Tile (7).png"/>
+ </tile>
+ <tile id="122">
+  <image width="256" height="256" source="../../Art/Tiles/Tile (8).png"/>
+ </tile>
+ <tile id="123">
+  <image width="256" height="256" source="../../Art/Tiles/Tile (9).png"/>
+ </tile>
+ <tile id="124">
+  <image width="256" height="256" source="../../Art/Tiles/Tile (10).png"/>
+ </tile>
+ <tile id="125">
+  <image width="256" height="256" source="../../Art/Tiles/Tile (11).png"/>
+ </tile>
+ <tile id="126">
+  <image width="256" height="256" source="../../Art/Tiles/Tile (12).png"/>
+ </tile>
+ <tile id="127">
+  <image width="256" height="256" source="../../Art/Tiles/Tile (13).png"/>
+ </tile>
+ <tile id="128">
+  <image width="256" height="256" source="../../Art/Tiles/Tile (14).png"/>
+ </tile>
+ <tile id="129">
+  <image width="256" height="256" source="../../Art/Tiles/Tile (15).png"/>
+ </tile>
+ <tile id="130">
+  <image width="256" height="256" source="../../Art/Tiles/Tile (16).png"/>
+ </tile>
+ <tile id="131">
+  <image width="256" height="256" source="../../Art/Tiles/Tile (17).png"/>
+ </tile>
+ <tile id="132">
+  <image width="256" height="256" source="../../Art/Tiles/Tile (18).png"/>
+ </tile>
+ <tile id="133">
+  <image width="256" height="256" source="../../Art/Tiles/Tile (19).png"/>
+ </tile>
+ <tile id="134">
+  <image width="256" height="256" source="../../Art/Tiles/Tile (20).png"/>
+ </tile>
+ <tile id="135">
+  <image width="256" height="256" source="../../Art/Tiles/Tile (21).png"/>
+ </tile>
+ <tile id="137">
+  <image width="256" height="256" source="../../Art/Tiles/Magnet Blue (1).png"/>
+ </tile>
+ <tile id="138">
+  <image width="256" height="256" source="../../Art/Tiles/Magnet Blue (2).png"/>
+ </tile>
+ <tile id="139">
+  <image width="256" height="256" source="../../Art/Tiles/Magnet Blue (3).png"/>
+ </tile>
+ <tile id="141">
+  <image width="256" height="256" source="../../Art/Tiles/Magnet Blue (4).png"/>
+ </tile>
+ <tile id="142">
+  <image width="256" height="256" source="../../Art/Tiles/Magnet Blue (5).png"/>
+ </tile>
+ <tile id="143">
+  <image width="256" height="256" source="../../Art/Tiles/Magnet Blue (6).png"/>
+ </tile>
+ <tile id="145">
+  <image width="256" height="256" source="../../Art/Tiles/Magnet Red (1).png"/>
+ </tile>
+ <tile id="146">
+  <image width="256" height="256" source="../../Art/Tiles/Magnet Red (2).png"/>
+ </tile>
+ <tile id="147">
+  <image width="256" height="256" source="../../Art/Tiles/Magnet Red (3).png"/>
+ </tile>
+ <tile id="148">
+  <image width="256" height="256" source="../../Art/Tiles/Magnet Red (4).png"/>
+ </tile>
+ <tile id="149">
+  <image width="256" height="256" source="../../Art/Tiles/Magnet Red (5).png"/>
+ </tile>
+ <tile id="150">
+  <image width="256" height="256" source="../../Art/Tiles/Magnet Red (6).png"/>
+ </tile>
+ <tile id="144">
+  <image width="256" height="256" source="../../Art/Tiles/input 4.png"/>
+ </tile>
+ <tile id="97">
+  <image width="256" height="256" source="../../Art/Tiles/input 1.png"/>
+ </tile>
+ <tile id="98">
+  <image width="256" height="256" source="../../Art/Tiles/input 2.png"/>
+ </tile>
+ <tile id="136">
+  <image width="256" height="256" source="../../Art/Tiles/input 3.png"/>
+ </tile>
+ <tile id="88">
+  <image width="256" height="256" source="../../Art/Tiles/input.png"/>
+ </tile>
+ <tile id="87">
+  <image width="256" height="256" source="../../Art/Tiles/None.png"/>
+ </tile>
+</tileset>
---
+++ Assets/Tiled/Tilesets/TilesObjects.tsx
@@ -0,0 +1,31 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="TilesObjects" tilewidth="357" tileheight="424" tilecount="9" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="11">
+  <image width="128" height="172" source="../../Art/Objects/Barrel (1).png"/>
+ </tile>
+ <tile id="12">
+  <image width="128" height="172" source="../../Art/Objects/Barrel (2).png"/>
+ </tile>
+ <tile id="13">
+  <image width="128" height="128" source="../../Art/Objects/Box.png"/>
+ </tile>
+ <tile id="14">
+  <image width="256" height="424" source="../../Art/Objects/DoorLocked.png"/>
+ </tile>
+ <tile id="15">
+  <image width="256" height="424" source="../../Art/Objects/DoorOpen.png"/>
+ </tile>
+ <tile id="16">
+  <image width="256" height="423" source="../../Art/Objects/DoorUnlocked.png"/>
+ </tile>
+ <tile id="17">
+  <image width="357" height="361" source="../../Art/Objects/Saw.png"/>
+ </tile>
+ <tile id="18">
+  <image width="72" height="257" source="../../Art/Objects/Switch (1).png"/>
+ </tile>
+ <tile id="19">
+  <image width="72" height="257" source="../../Art/Objects/Switch (2).png"/>
+ </tile>
+</tileset>
